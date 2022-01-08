09e63b1260eda4f0b500919a94a4c22e0b75cc7c
FXR <kay_op@protonmail.com>
2022-01-08 01:00:36 -0600

    Start ignoring .history from git

--- .history/hooks/pre_gen_project_20220107181810.py
+++
@@ -1,9 +0,0 @@
-import os
-
-project_slug = "{{cookiecutter.project_slug}}"
-
-ERROR_COLOR = "\x1b[31m"
-MESSAGE_COLOR = "\x1b[34m"
-RESET_ALL = "\x1b[0m"
-
-print(f"{MESSAGE_COLOR}Creating '{project_slug}' at {os.getcwd()}{RESET_ALL}")
